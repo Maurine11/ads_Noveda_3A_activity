7711d497067bcf7a31ccd101bba3c59acd698054
adsnoveda <maurinenoveda7@gmail.com>
2024-09-24 01:49:26 -0700

    Completed Activity 3: Eloquent ORM Basics

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
+    protected $fillable = ['first_name', 'last_name', 'email', 'age'];
+    public function students()
+    {
+    	return $this->belongsTo(Course::class);
+    } 
     use HasFactory;
-    protected $fillable = ['first_name', 'last_name', 'email', 'age'] ;
-  }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,53 @@
 <?php
 
+use App\Models\Student;
+use App\Models\course;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/students/create', function () {
+	$student = new Student();
+	$student->first_name = 'John';
+	$student->last_name = 'Doe';
+	$student->email = 'johndoe@example.com';
+	$student->age = 22;
+	$student->save();
+	return 'Student Created!';
+});
+
+Route::get('/students', function () {
+	$students = student::all();
+	return $students;
+});
+
+Route::get('/students/update', function () {
+	$student = student::where('email', 'johndoe@example.com')->first();
+	$student->email = 'john.doe@newmail.com';
+	$student->age = 23;
+	$student->save();
+	return 'Student Updated!';
+});
+
+Route::get('/students/delete', function () {
+	$student = student::where('email', 'john.doe@newmail.com')->first();
+	$student->delete();
+	return 'Student Deleted!';
+});
+
+Route::get('/courses/create', function () {
+	$course = new Course();
+	$course->course_name = 'Introduction to Databases';
+	$course->save();
+	return 'Course Crteated!';
+});
+
+Route::get('/course/{id}/students', function ($id) {
+	$course = Course::find($id);
+	return $course->student;
+});
 
 
 /*
@@ -13,7 +60,3 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
